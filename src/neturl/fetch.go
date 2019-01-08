4295232df1901ee18fbe4f10b68d1908a1812eb5
yangwu <yangwu@sanganvideo.com>
2019-01-08 14:39:22 +0800

    add net fetch

--- src/neturl/fetch.go
+++ src/neturl/fetch.go
@@ -7,14 +7,17 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
-	for _, url := range os.Args[1:] {
+	
+	concurFetch()
+	/*for _, url := range os.Args[1:] {
 
 		rturl := geturl(url)
 		resp, err := http.Get(rturl)
-		
+
 		fmt.Println("get url status:" + resp.Status)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
@@ -26,15 +29,14 @@ func main() {
 			os.Exit(1)
 		}
 		fmt.Printf("length of body:%d", count)
-		/*
 			b, err := getBodyByIOUtil(resp)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 				os.Exit(1)
 			}
 			fmt.Printf("content of url: %s = \n%s", url, b)
-		*/
-	}
+		
+	}*/
 }
 
 func getBodyByIOUtil(resp *http.Response) ([]byte, error) {
@@ -51,9 +53,41 @@ func getBodyByIOCopy(resp *http.Response) (int64, error) {
 }
 func geturl(url string) string {
 	fmt.Println("init url = " + url)
-	if (!strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://")) {
+	if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
 		url = "http://" + url
 	}
 	fmt.Println("final url = " + url)
 	return url
+}
+
+func concurFetch() {
+	start := time.Now()
+	ch := make(chan string)
+	for _,url :=range os.Args[1:]{
+		go fetch(url,ch)	
+	}
+	
+	for index,_ := range os.Args[1:]{
+		fmt.Printf("current index: %d, time: %d |  ", index,time.Now().Nanosecond());
+		fmt.Println(<-ch)
+	}
+	fmt.Printf("%.2fs elapsed \n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string){
+	start := time.Now()
+	resp,err := http.Get(url)
+	
+	if err != nil{
+		ch <- fmt.Sprint(err)
+	}
+	nbytes,err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
+	if err != nil{
+		ch <-fmt.Sprintf("while reading %s:%v", url,err);
+		return 
+	}
+	secs := time.Since(start).Seconds()
+	
+	ch<-fmt.Sprintf("%.2fs    %7d    %s", secs,nbytes, url)
 }
